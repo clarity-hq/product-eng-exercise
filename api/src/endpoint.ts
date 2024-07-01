e55d2358bb5f0229fe59bb950dd0a516caf51e45
alexbckr <amb3yn@virginia.edu>
2024-07-01 03:12:54 -0400

    date filtering on the server

--- api/src/endpoint.ts
+++ api/src/endpoint.ts
@@ -1,23 +1,38 @@
-import bodyParser from "body-parser";
-import express, { Request, Response } from "express";
-import json from "./data.json";
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import json from './data.json';
 
 type Feedback = {
   name: string;
   description: string;
-  importance: "High" | "Medium" | "Low";
-  type: "Sales" | "Customer" | "Research";
-  customer: "Loom" | "Ramp" | "Brex" | "Vanta" | "Notion" | "Linear" | "OpenAI";
+  importance: 'High' | 'Medium' | 'Low';
+  type: 'Sales' | 'Customer' | 'Research';
+  customer: 'Loom' | 'Ramp' | 'Brex' | 'Vanta' | 'Notion' | 'Linear' | 'OpenAI';
   date: string;
 };
 
 type FeedbackData = Feedback[];
-type FeedbackFilter = Partial<Feedback>;
+
+type AttributeGranularFilter<T> = T[];
+type AttributeFilter<T> = {
+  include?: AttributeGranularFilter<T>;
+  exclude?: AttributeGranularFilter<T>;
+};
+type FeedbackFilter = {
+  name?: AttributeFilter<string>;
+  description?: AttributeFilter<string>;
+  importance?: AttributeFilter<'High' | 'Medium' | 'Low'>;
+  type?: AttributeFilter<'Sales' | 'Customer' | 'Research'>;
+  customer?: AttributeFilter<
+    'Loom' | 'Ramp' | 'Brex' | 'Vanta' | 'Notion' | 'Linear' | 'OpenAI'
+  >;
+  date?: AttributeFilter<string>;
+};
 
 export const router = express.Router();
 router.use(bodyParser.json());
 
-router.post("/query", queryHandler);
+router.post('/query', queryHandler);
 
 function queryHandler(req: Request, res: Response<{ data: FeedbackData }>) {
   const data: FeedbackData = json as any;
@@ -32,22 +47,52 @@ function filterData(data: FeedbackData, filter: FeedbackFilter): FeedbackData {
   return data.filter((feedback) =>
     Object.keys(filter).every((key) => {
       const attributeKey = key as keyof Feedback;
-      const feedbackAttribute = feedback[attributeKey];
       const filterItem = filter[attributeKey];
 
-      if (filterItem === undefined) {
+      if (!filterItem) {
         return true;
       }
+      const { include, exclude } = filterItem;
+      const feedbackValue = feedback[attributeKey];
 
-      // partial matches
-      if (attributeKey === "name" || attributeKey === "description") {
-        return feedbackAttribute
-          .toLowerCase()
-          .includes(filterItem.toLocaleLowerCase());
-      }
+      const includeCheck =
+        ignoreFilter(include) ||
+        include?.some((includedValue) =>
+          matches(attributeKey, feedbackValue, includedValue)
+        );
+      const excludeCheck =
+        ignoreFilter(exclude) ||
+        !exclude?.every((excludedValue) =>
+          matches(attributeKey, feedbackValue, excludedValue)
+        );
 
-      // full match
-      return feedbackAttribute === filterItem;
+      return includeCheck && excludeCheck;
     })
   );
+}
+
+function ignoreFilter(filter?: AttributeGranularFilter<any>) {
+  return !filter || filter.length === 0;
+}
+
+function matches(
+  attributeKey: string,
+  value: string,
+  comparison: string
+): boolean {
+  const softMatch = ['name', 'description'].includes(attributeKey);
+
+  if (attributeKey === 'date') {
+    const valueDate = new Date(value);
+    const comparisonDate = new Date(comparison);
+    // matches dates, ignoring times
+    return (
+      valueDate.toISOString().slice(0, 10) ===
+      comparisonDate.toISOString().slice(0, 10)
+    );
+  } else if (softMatch) {
+    return value.toLowerCase().includes(comparison.toLowerCase());
+  } else {
+    return value.toLowerCase() === comparison.toLowerCase();
+  }
 }
